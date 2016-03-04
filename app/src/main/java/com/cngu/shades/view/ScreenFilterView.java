f4898f1387afa70fdd8b5fa8452961e7d48e79a9
Marien Raat <raatmarien@gmail.com>
2016-03-04 19:21:44 +0100

    Change alpha and color calculations of the filter
    It now factors in the dim factor and the intensity of the color. This
    allows for more precise color/darkness combinations. The intensity can
    not be changed from the application yet.

--- app/src/main/java/com/cngu/shades/view/ScreenFilterView.java
+++ app/src/main/java/com/cngu/shades/view/ScreenFilterView.java
@@ -14,9 +14,14 @@ public class ScreenFilterView extends View {
     private static final float MAX_DIM   = 100f;
     private static final float MIN_ALPHA = 0x00;
     private static final float MAX_ALPHA = (int) (0xFF * 0.75);
+    private static final float MAX_DARKEN = 0.75f;
+
+    private static final float DIM_ALPHA_INFLUENCE       = 0.5f;
+    private static final float INTENSITY_ALPHA_INFLUENCE = 0.5f;
 
     private int mDimLevel = DimSeekBarPreference.DEFAULT_VALUE;
-    private int mAlpha = dimLevelToAlpha(mDimLevel);
+    private int mIntensityLevel = 50; // TODO: default value
+    private int mAlpha = levelsToAlpha(mDimLevel, mIntensityLevel);
     private int mRgbColor = ColorPickerPreference.DEFAULT_VALUE;
 
     public ScreenFilterView(Context context) {
@@ -34,13 +39,26 @@ public class ScreenFilterView extends View {
     /**
      * Sets the dim level of the screen filter.
      *
-     * @param dimLevel value between 0 and 100, inclusive, where 0 is fully transparent, and 100 is
-     *                 the maximum allowed dim level determined by the system, but is guaranteed to
+     * @param dimLevel value between 0 and 100, inclusive, where 0 is doesn't darken, and 100 is the
+     *                 maximum allowed dim level determined by the system, but is guaranteed to
      *                 never be fully opaque.
      */
     public void setFilterDimLevel(int dimLevel) {
         mDimLevel = dimLevel;
-        mAlpha = dimLevelToAlpha(dimLevel);
+        mAlpha = levelsToAlpha(mDimLevel, mIntensityLevel);
+        invalidate();
+    }
+
+    /**
+     * Sets the intensity of the screen filter.
+     *
+     * @param intensityLevel value between 0 and 100, inclusive, where 0 doesn't color the filter,
+     *                       and 100 is the maximum allowed intensity determined by the system, but
+     *                       is guaranteed to never be fully opaque.
+     */
+    public void setFilterIntensityLevel(int intensityLevel) {
+        mIntensityLevel = intensityLevel;
+        mAlpha = levelsToAlpha(mDimLevel, mIntensityLevel);
         invalidate();
     }
 
@@ -57,11 +75,17 @@ public class ScreenFilterView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.argb(mAlpha, Color.red(mRgbColor), Color.green(mRgbColor), Color.blue(mRgbColor)));
+        float darken = (MAX_DIM - (mDimLevel * MAX_DARKEN)) / MAX_DIM;
+        canvas.drawColor(Color.argb(mAlpha,
+                                    (int) ((float) Color.red(mRgbColor) * darken),
+                                    (int) ((float) Color.green(mRgbColor) * darken),
+                                    (int) ((float) Color.blue(mRgbColor) * darken)));
     }
 
-    private static int dimLevelToAlpha(int dimLevel) {
-        return (int) mapToRange((float) dimLevel, MIN_DIM, MAX_DIM, MIN_ALPHA, MAX_ALPHA);
+    private static int levelsToAlpha(int dimLevel, int intensityLevel) {
+        float totalRelativeValue = ((float) dimLevel) * DIM_ALPHA_INFLUENCE +
+            ((float) intensityLevel) * INTENSITY_ALPHA_INFLUENCE;
+        return (int) mapToRange(totalRelativeValue, MIN_DIM, MAX_DIM, MIN_ALPHA, MAX_ALPHA);
     }
 
     private static float mapToRange(float value, float minInput, float maxInput,
