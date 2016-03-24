427ad6eecec3e3a5b0a8941f88240e1a95558151
Marien Raat <raatmarien@gmail.com>
2016-03-24 15:18:51 +0100

    Only show warning toast on filter open

--- app/src/main/java/com/jmstudios/redmoon/activity/ShadesActivity.java
+++ app/src/main/java/com/jmstudios/redmoon/activity/ShadesActivity.java
@@ -120,7 +120,8 @@ public class ShadesActivity extends AppCompatActivity {
                         mPresenter.sendCommand(isChecked ?
                                                ScreenFilterService.COMMAND_ON :
                                                ScreenFilterService.COMMAND_OFF);
-                        displayInstallWarningToast();
+                        if (isChecked)
+                            displayInstallWarningToast();
                     }
                 }
             });
