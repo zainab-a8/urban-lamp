dadf619d7b797f8107d236a6f21f7f281e086406
Joona <joonatoona@digitalfishfun.com>
2017-08-23 14:50:21 -0700

    Add root listener source
    `listener.cpp`, when compiled for the right architecture, will listen on an existing fifo at `argv[1]`.
    Whenever something is recived on the fifo, it will run `service call SurfaceFlinger <line>`.
    To kill the listener, write `exit` to the fifo.

---
+++ root-listener/listener.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+	if (argc < 2)
+	{
+		std::cout << "Usage: " << argv[0] << " <path/to/fifo>\n";
+		return 1;
+	}
+	
+	std::fstream f_pipe(argv[1]);
+	for (std::string line; std::getline(f_pipe, line);)
+	{
+		if (line == "exit")
+		{
+			std::cout << "Recived exit!\n";
+			break;
+		}
+		
+		std::string cmd("service call SurfaceFlinger ");
+		cmd.append(line);
+		popen(cmd.c_str(), "r");
+	}
+	
+	std::cout << "Goodbye!\n";
+	return 0;
+}
